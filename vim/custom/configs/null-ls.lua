b089db3e3a0d85089af9badc9b68fd3579098636
Yoshihiro Nagano <y.nagano.92@gmail.com>
2023-07-11 22:11:41 +0900

    update vim setting

--- vim/custom/configs/null-ls.lua
+++ vim/custom/configs/null-ls.lua
@@ -6,7 +6,11 @@ null_ls.setup {
     null_ls.builtins.diagnostics.mypy.with({
       prefer_local = ".venv/bin",
     }),
-    null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.ruff.with({
+      prefer_local = ".venv/bin",
+    }),
+    null_ls.builtins.formatting.black.with({
+      prefer_local = ".venv/bin",
+    }),
   }
 }
