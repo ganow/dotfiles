e1b9932126047593c6adbb7c66aae33babbd8a84
Yoshihiro Nagano <y.nagano.92@gmail.com>
2026-03-10 13:39:14 +0900

    update transparency settings for transparent background in neovim editor

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
+      show_end_of_buffer = false,
+      term_colors = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
